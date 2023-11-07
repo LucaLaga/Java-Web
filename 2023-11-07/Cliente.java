cf3732828495ae7cd078109057081b92553977fb
Luca Laganà <lucalagana998@gmail.com>
2023-11-07 16:10:30 +0100

    Chunk1: Cliente

---
+++ 2023-11-07/Cliente.java
@@ -0,0 +1,45 @@
+public class Cliente {
+    private final int id;
+    private String nome;
+    private String email;
+    public static int nextId; 
+
+    public Cliente(String nome, String email){
+        this.id = nextId++;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    // Getters
+    public int getId(){
+        return this.id;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    // Setters
+    public void setNome(String nome){
+        if(nome!=null){
+            this.nome = nome;
+        }
+    }
+
+    public void setEmail(String email){
+        if(nome!=null){
+            this.email = email;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return this.id + ": "
+        + this.nome + " - "
+        + this.email;
+    }
+}
