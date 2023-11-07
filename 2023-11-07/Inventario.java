9cd1ff3460ab81fad6cebc6fce54ceeda87b5500
Luca Laganà <lucalagana998@gmail.com>
2023-11-07 16:40:53 +0100

    Chunk 2

---
+++ 2023-11-07/Inventario.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventario {
+    private List<Giocattolo> giocattoli = new ArrayList<>();
+    private Cliente cliente;
+
+    public Inventario(Cliente cliente){
+        if(cliente != null){
+            this.cliente = cliente;
+        }
+    }
+
+    public void aggiungiGiocattolo(Giocattolo giocattolo){
+        if(giocattolo != null & this.cliente.checkAdmin()){
+            this.giocattoli.add(giocattolo);
+        }
+    }
+}
