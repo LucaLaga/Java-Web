e645d8ae3777fc5b0148e6159b9e476cbc2f522c
Luca Laganà <lucalagana998@gmail.com>
2023-11-08 12:37:58 +0100

    Chunk3: Update RegistroVendite

--- 2023-11-07/RegistroVendite.java
+++ 2023-11-07/RegistroVendite.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 public abstract class RegistroVendite {
-    public List<Vendita> vendi(Cliente cliente, Giocattolo giocattolo, List<Vendita> vendite){
+    public static List<Vendita> vendi(Cliente cliente, Giocattolo giocattolo, List<Vendita> vendite){
         if(cliente!=null && giocattolo!=null){
             Vendita vendita = new Vendita(giocattolo, cliente);
             vendita.acquista();
