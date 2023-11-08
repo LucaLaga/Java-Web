bf5c44c9c879c147fc05ad9f148be4ce0d0840ea
Luca Laganà <lucalagana998@gmail.com>
2023-11-08 12:19:17 +0100

    Chunk1: Update Vendita

--- 2023-11-07/Vendita.java
+++ 2023-11-07/Vendita.java
@@ -1,21 +1,15 @@
 public class Vendita {
     private Giocattolo giocattolo;
     private Cliente cliente;
-    private String tipo;
 
-    public Vendita(Giocattolo giocattolo, Cliente cliente, String tipo){
+    public Vendita(Giocattolo giocattolo, Cliente cliente){
         if(giocattolo != null & cliente != null){
             this.cliente = cliente;
             this.giocattolo = giocattolo;
-            this.tipo = tipo;
         }
     }
 
     public void acquista(){
         cliente.aggiornaConto(cliente.getConto()-giocattolo.getPrezzo());
-    }
-
-    public void vendi(){
-        cliente.aggiornaConto(cliente.getConto()+giocattolo.getPrezzo());
     }
 }
