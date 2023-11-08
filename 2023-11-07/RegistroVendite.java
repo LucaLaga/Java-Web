afd8885a99934460fb8542b7f272be5c210eb56f
Luca Laganà <lucalagana998@gmail.com>
2023-11-08 12:19:43 +0100

    Chunk3: Update Vendita

--- 2023-11-07/RegistroVendite.java
+++ 2023-11-07/RegistroVendite.java
@@ -1,25 +1,13 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class RegistroVendite {
-    protected static List<Vendita> vendite = new ArrayList<>();
-    public static void acquista(Cliente cliente, Giocattolo giocattolo){
-        System.out.println("Stai acquistando un " + giocattolo.getNome());
-        updateVendite(cliente, giocattolo, "acquisto");
-    }
-    public static void vendi(Cliente cliente, Giocattolo giocattolo){
-        System.out.println("Stai vendendo un " + giocattolo.getNome());
-        updateVendite(cliente, giocattolo, "vendita");
-    }
-
-    private static void updateVendite(Cliente cliente, Giocattolo giocattolo, String tipo){
-        Vendita newVendita = new Vendita(giocattolo, cliente, tipo);
-        if(tipo.equals("acquisto")){
-            newVendita.acquista();
-        }else{
-            newVendita.vendi();
+    public List<Vendita> vendi(Cliente cliente, Giocattolo giocattolo, List<Vendita> vendite){
+        if(cliente!=null && giocattolo!=null){
+            Vendita vendita = new Vendita(giocattolo, cliente);
+            vendita.acquista();
+            vendite.add(vendita);
         }
-        
-        vendite.add(newVendita);
+
+        return vendite;
     }
 }
